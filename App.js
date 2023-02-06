317d8d0350f1d34a5b24d982d4ec2e1ae9c10695
jjugwen <yjgwen@gmail.com>
2023-02-06 22:44:03 +0900

    Update App.js
    1 위치 동의 안한 경우 UI
    2 정렬

--- App.js
+++ App.js
@@ -1,68 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
-import * as Location from 'expo-location';
-import {APP_WEATHERAPI} from "@env"
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import * as Location from "expo-location";
+import { APP_WEATHERAPI } from "@env";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
-const API_KEY = APP_WEATHERAPI
+const API_KEY = APP_WEATHERAPI;
 
 export default function App() {
-  const [city, setCity] = useState("Loading...")
-  const [days, setDays] = useState([])
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const getWeather = async() => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
-    if(!granted){
-      setOk(false)
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
     }
-    const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5})
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps: false})
-    setCity(location[0].city)
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
     const json = await response.json();
-    setDays(json.list.filter((weather) => {
-      if(weather.dt_txt.includes("00:00:00")){
-        return weather;
-      }
-    }))
-  }
-  useEffect(()=> {
+    setDays(
+      json.list.filter((weather) => {
+        if (weather.dt_txt.includes("00:00:00")) {
+          return weather;
+        }
+      })
+    );
+  };
+  useEffect(() => {
     getWeather();
-  })
+  });
 
   return (
     <View style={styles.container}>
-      <StatusBar style='light' />
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
-      </View>
-      <ScrollView 
-        horizontal 
-        pagingEnabled 
-        showsHorizontalScrollIndicator={false}
-        // indicatorStyle='white'
-        contentContainerStyle={styles.weather}>
-        
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white" style={{marginTop: 10}} size="large" />
+      <StatusBar style="light" />
+      {ok ? (
+        <>
+          <View style={styles.city}>
+            <Text style={styles.cityName}>{city}</Text>
           </View>
-        ) :(
-          days.map((day, index)=>(
-        <View key={index} style={styles.day}>
-          <Text style={styles.temp}>
-            {parseFloat(day.main.temp).toFixed(1)}
-          </Text>
-          <Text style={styles.description}>{day.weather[0].main}</Text>
-          <Text style={styles.tinyText}>{day.weather[0].description}</Text>
-          <Text style={styles.tinyText}>{day.dt_txt.slice(0, 10)}</Text>
-        </View>
-          )) 
-        )
-        }
-      </ScrollView>
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            // indicatorStyle='white'
+            contentContainerStyle={styles.weather}
+          >
+            {days.length === 0 ? (
+              <View style={styles.day}>
+                <ActivityIndicator
+                  color="white"
+                  style={{ marginTop: 10 }}
+                  size="large"
+                />
+              </View>
+            ) : (
+              days.map((day, index) => (
+                <View key={index} style={styles.day}>
+                  <Text style={styles.temp}>
+                    {parseFloat(day.main.temp).toFixed(1)}
+                  </Text>
+                  <Text style={styles.description}>{day.weather[0].main}</Text>
+                  <Text style={styles.tinyText}>
+                    {day.weather[0].description}
+                  </Text>
+                  <Text style={styles.tinyText}>{day.dt_txt.slice(0, 10)}</Text>
+                </View>
+              ))
+            )}
+          </ScrollView>
+        </>
+      ) : (
+        <>
+          <View style={{ flex: 1 }}>
+            <View style={styles.city}>
+              <Text style={styles.description}>Are you sure?</Text>
+            </View>
+          </View>
+        </>
+      )}
     </View>
   );
 }
@@ -70,16 +103,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "tomato"
+    backgroundColor: "tomato",
   },
   city: {
     flex: 1.2,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   cityName: {
     fontSize: 58,
-    fontWeight: "500"
+    fontWeight: "500",
   },
   weather: {
     // backgroundColor: "blue"
@@ -91,13 +124,13 @@ const styles = StyleSheet.create({
   temp: {
     marginTop: 50,
     fontWeight: "600",
-    fontSize: 178
+    fontSize: 178,
   },
   description: {
     marginTop: -30,
-    fontSize: 60
+    fontSize: 60,
   },
   tinyText: {
-    fontSize: 20
-  }
-})
+    fontSize: 20,
+  },
+});
